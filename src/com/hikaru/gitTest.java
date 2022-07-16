6aa5173e3499ae9d00370d376f26f17c3cea06bb
‘tod4-lab’ <819574539@qq.com>
2022-07-16 23:50:51 +0800

    hotfix commit

--- src/com/hikaru/gitTest.java
+++ src/com/hikaru/gitTest.java
@@ -2,12 +2,12 @@ package com.hikaru;
 
 public class gitTest {
     public static void main(String[] args) {
-        System.out.println("Hello git1");
+        System.out.println("Hello git6");
 
-        System.out.println("Hello git2");
-        System.out.println("Hello git2");
-        System.out.println("Hello git3");
-        System.out.println("hotfix git3");
+        System.out.println("Hello git6");
+        System.out.println("Hello git6");
+        System.out.println("Hello git6");
+        System.out.println("hotfix git6");
         System.out.println("hotfix git6");
     }
 }
