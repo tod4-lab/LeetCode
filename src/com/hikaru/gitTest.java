e9dd47b06ab131ad73191c22787de2e1d8fd4d16
‘tod4-lab’ <819574539@qq.com>
2022-07-16 23:30:44 +0800

    master commit

--- src/com/hikaru/gitTest.java
+++ src/com/hikaru/gitTest.java
@@ -7,5 +7,6 @@ public class gitTest {
         System.out.println("Hello git2");
         System.out.println("Hello git2");
         System.out.println("Hello git3");
+        System.out.println("master commit");
     }
 }
