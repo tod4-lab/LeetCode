7aaa65de670610750cd479f0d2f3a6aab38326a9
‘tod4-lab’ <819574539@qq.com>
2022-07-16 23:24:11 +0800

    hot-fix commit

--- src/com/hikaru/gitTest.java
+++ src/com/hikaru/gitTest.java
@@ -6,5 +6,6 @@ public class gitTest {
         System.out.println("Hello git2");
         System.out.println("Hello git2");
         System.out.println("Hello git2");
+        System.out.println("Hello git3");
     }
 }
